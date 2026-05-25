ee4c92872e8f3ce85b36205aadd4769116b55b09
OhanaMGH <ohanagutierrez229@gmail.com>
2026-05-24 20:42:52 -0600

    Cambios en translate service

--- backend/services/translate_service.py
+++ backend/services/translate_service.py
@@ -1,19 +1,32 @@
 from transformers import MarianMTModel, MarianTokenizer
 
-print("Cargando traductor inglés → español...")
+MODEL_NAME = "Helsinki-NLP/opus-mt-en-es"
 
-tokenizer = MarianTokenizer.from_pretrained(
-    "Helsinki-NLP/opus-mt-en-es"
-)
+tokenizer = None
+model = None
 
-model = MarianMTModel.from_pretrained(
-    "Helsinki-NLP/opus-mt-en-es"
-)
+
+def cargar_modelo():
+    global tokenizer, model
+
+    if tokenizer is None or model is None:
+        print("Cargando traductor inglés → español...")
+
+        tokenizer = MarianTokenizer.from_pretrained(MODEL_NAME)
+
+        model = MarianMTModel.from_pretrained(MODEL_NAME)
+
+        print("Traductor cargado correctamente")
 
 
 def traducir_es(texto):
+    cargar_modelo()
 
-    inputs = tokenizer(texto, return_tensors="pt", padding=True)
+    inputs = tokenizer(
+        texto,
+        return_tensors="pt",
+        padding=True
+    )
 
     translated = model.generate(**inputs)
 
