c843ac379640c8cabb6148c719633e3fdfdfd14c
OhanaMGH <ohanagutierrez229@gmail.com>
2026-05-24 20:04:31 -0600

    Conexion Mongo Atlas

--- backend/database.py
+++ backend/database.py
@@ -1,21 +1,28 @@
 from pymongo import MongoClient
+import os
 import sys
 
-# Configuración de conexión
-MONGO_URI = "mongodb://localhost:27017/"
+# URI obtenida desde Render Environment Variables
+MONGO_URI = os.getenv("MONGO_URI")
 
 try:
     client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
-    # Según tu imagen, la base de datos es "REIF" y la colección es "imagenes"
+
+    # Base de datos
     db = client["REIF_test"]
+
+    # Colección
     coleccion_imagenes = db["imagenes"]
-    
-    # Prueba de conexión rápida
-    client.server_info() 
-    print("Conexión exitosa a MongoDB: REIF > imagenes")
+
+    # Verificar conexión
+    client.server_info()
+
+    print("✅ Conexión exitosa a MongoDB Atlas")
+
 except Exception as e:
-    print(f"Error al conectar a MongoDB: {e}")
+    print(f"❌ Error al conectar a MongoDB Atlas: {e}")
     sys.exit(1)
+
 
 def obtener_coleccion():
     return coleccion_imagenes
